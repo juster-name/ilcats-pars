78d67a43e9c6ac2da7a70bd0a6bcb22e07f6d3ba
juster-name <vladlen044@gmail.com>
2023-01-05 17:21:12 +0200

    all string data

--- CarModel.cs
+++ CarModel.cs
@@ -8,7 +8,7 @@ namespace VladlenKazmiruk
         public string? DateStart { get => dateStart; set => dateStart = value; }
         public string? DateEnd { get => dateEnd; set => dateEnd = value; }
         public string? ComplectationCode { get => complectationCode; set => complectationCode = value; }
-        public int? Code { get => code; set => code = value; }
+        public string? Code { get => code; set => code = value; }
         public HashSet<Complectation>? Complectations { get => complectation; set => complectation = value; }
         public Car? Car { get => car; set => car = value; }
         public string? Url { get => url; set => url = value; }
--- Catalog_0.cs
+++ Catalog_0.cs
@@ -5,7 +5,7 @@ namespace VladlenKazmiruk
     public class Catalog_0 : DomElementContainer
     {
         public string? Name { get => name; set => name = value; }
-        public int? Complectation_id { get => complectation_id; set => complectation_id = value; }
+        public string? Complectation_id { get => complectation_id; set => complectation_id = value; }
         public HashSet<Catalog_1>? Catalogs_1 { get => catalogs_1; set => catalogs_1 = value; }
         public Complectation? Complectation { get => complectation; set => complectation = value; }
 
