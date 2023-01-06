540b233e0101001f749f8d78368c26769ff1be66
juster-name <vladlen044@gmail.com>
2023-01-06 22:16:35 +0200

    redundant selectors in Data.Car

--- Data/Car.cs
+++ Data/Car.cs
@@ -10,15 +10,6 @@ namespace VladlenKazmiruk
 
             public string? Name { get => name; set => name = value; }
             public IEnumerable<CarModel> Models { get => models; set => models = value; }
-
-            public static string topLevelSelector = ".Multilist";
-            public static string carCellSelector = ":scope > div[class='List']"; // :scope для topLevelSelector
-
-            public static string carNameSelector = "div[class='name']";
-            public static string carInfoSelector = "div[class='List'] div[class='List']";
-            public static string carDatesSelector = "div[class='dateRange']";
-            public static string carComplCodeSelector = "div[class='modelCode']";
-
             private IEnumerable<CarModel> models = Enumerable.Empty<CarModel>();
             string? name = null;
         }
