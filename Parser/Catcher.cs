4283a8755598c740c7bd2943fc5179ecfe674371
juster-name <vladlen044@gmail.com>
2023-01-06 14:21:38 +0200

    CarCatcher implementation

--- Parser/Catcher.cs
+++ Parser/Catcher.cs
@@ -8,24 +8,62 @@ namespace VladlenKazmiruk
         // Интерфейс для получения Data класса <T>
         interface ICatcher<T>
         {
+            public IEnumerable<T> Catch(); // Поиск и получения Data класса <T> по элементу
+
             public IElement ContextElement { get; } // Родительский dom элемент для осуществления поиска
-            public IElement getElement(); // Top-level dom элемент для поиска <T>
-            public T Catch(); // Поиск и получения Data класса <T> по элементу
+            public IElement? CurrentElement { get; } // Nекущий dom элемент из Catch() (yield return)
+
+            public IElement getTopElement(); // Top-level dom элемент для поиска <T>
         }
 
         public abstract class BaseCather<T> : ICatcher<T>
         {
             public IElement ContextElement { get => contextElement; }
-            IElement contextElement;
-            protected IElement? element = null;
+            public IElement? CurrentElement { get => currentElement; } 
 
-            public BaseCather(IElement contextElement)
+            IElement contextElement; // Используется только в конструкторе, контекст изменить нельзя
+
+            protected IElement? currentElement = null; // Присваиваем значение во время работы метода Catch()
+            protected IElement? topElement = null; // Ленивая инициализация из метода getTopElement() либо Catch()
+
+            public BaseCather(IElement contextElement) // Обяхательный родительский элемент (контекст) при инициализации
             {
                 this.contextElement = contextElement;
             }
+            abstract public IElement getTopElement();
+            abstract public IEnumerable<T> Catch();
+        }
 
-            abstract public IElement getElement();
-            abstract public T Catch();
+        public class CarsCatcher : BaseCather<Data.Car>
+        {
+
+            public CarsCatcher(IElement contextElement) : base(contextElement)
+            {
+            }
+            public override IEnumerable<Data.Car> Catch()
+            {
+                var carCells = this.getTopElement().QuerySelectorAll(Selectors.carCellSelector);
+
+                foreach (var cellEl in carCells)
+                {
+                    var car = new Data.Car();
+                    var carNameEl = cellEl.QuerySelector(Selectors.carNameSelector);
+                    car.Name = carNameEl?.TextContent;
+                    base.currentElement = carNameEl;
+                    yield return car;
+                }
+            }
+
+            public override IElement getTopElement()
+            {
+                if (base.topElement == null)
+                {
+                    // Possible Null Reference от QuerySelector не актуален - элемент всегда присутствует.
+                    base.topElement = base.ContextElement.QuerySelectorAll(Selectors.carTopLevelSelector)[0];
+                    return base.topElement;
+                }
+                return base.topElement;
+            }
         }
 
     }
--- Parser/Selectors.cs
+++ Parser/Selectors.cs
@@ -7,7 +7,7 @@ namespace VladlenKazmiruk
     {
         public static class Selectors
         {
-            public static string topLevelSelector = ".Multilist";
+            public static string carTopLevelSelector = ".Multilist";
             public static string carCellSelector = ":scope > div[class='List']"; // :scope для topLevelSelector
             public static string carNameSelector = "div[class='name']";
             public static string carInfoSelector = "div[class='List'] div[class='List']";
