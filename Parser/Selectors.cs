0ce26f644cbc7a64978381b3c9a3bfdc80341d04
juster-name <vladlen044@gmail.com>
2023-01-06 22:16:05 +0200

    proper selector for CarModel

--- Parser/Selectors.cs
+++ Parser/Selectors.cs
@@ -10,8 +10,8 @@ namespace VladlenKazmiruk
             public static string carTopLevelSelector = ".Multilist";
             public static string carCellSelector = ":scope > div[class='List']"; // :scope для topLevelSelector
             public static string carNameSelector = "div[class='name']";
-            public static string carInfoTopSelector = "div[class='List']";
-            public static string carInfoSelector = carInfoTopSelector + "div[class='List']";
+            public static string carInfoTopSelector = ":scope > div[class='List ']"; // : scope для carCellSelector
+            public static string carInfoSelector =  "div[class='List']";
             public static string carDatesSelector = "div[class='dateRange']";
             public static string carComplCodeSelector = "div[class='modelCode']";
         }
