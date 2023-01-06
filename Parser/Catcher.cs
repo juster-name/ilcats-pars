d286b40987bf34441c3ac307e61aba47f19d6ae3
juster-name <vladlen044@gmail.com>
2023-01-06 19:48:45 +0200

    Catch method in BaseCatcher for abstract parsing

--- Parser/Catcher.cs
+++ Parser/Catcher.cs
@@ -11,9 +11,10 @@ namespace VladlenKazmiruk
             public IEnumerable<T> Catch(); // Поиск и получения Data класса <T> по элементу
 
             public IElement ContextElement { get; } // Родительский dom элемент для осуществления поиска
-            public IElement? CurrentElement { get; } // Nекущий dom элемент из Catch() (yield return)
+            public IElement? CurrentElement { get; } // Текущий dom элемент из Catch() (yield return)
 
             public IElement getTopElement(); // Top-level dom элемент для поиска <T>
+            public IHtmlCollection<IElement> getElementCollection(); // Все элементы из topElement
         }
 
         public abstract class BaseCather<T> : ICatcher<T>
@@ -25,31 +26,61 @@ namespace VladlenKazmiruk
 
             protected IElement? currentElement = null; // Присваиваем значение во время работы метода Catch()
             IElement? topElement = null; // Ленивая инициализация из метода getTopElement() либо Catch()
+            IHtmlCollection<IElement>? elementCollection = null; // Аналогично предыдущему полю
 
-            public BaseCather(IElement? contextElement) // Обяхательный родительский элемент (контекст) при инициализации
+            public BaseCather(IElement? contextElement) // Обязательный родительский элемент (контекст) при инициализации
             {
                 if (contextElement is null)
                 {
-                    throw new NullReferenceException();
+                    throw new NullReferenceException("contextElement must not be null");
                 }
 
                 this.contextElement = contextElement;
             }
-            public IElement getTopElement() // Проверка на NullReference
-            {
-                if (this.topElement is null) // Если не инициализированна
-                {
-                    this.topElement = this.locateTopElement();
 
-                    if (this.topElement is null) // Если не нашли элемент
+            public IElement getTopElement() // Проверка на NullReference и инициализация по требованию
+            {
+                return this.initIfNotNull(this.topElement, this.locateTopElement);
+            }
+            
+            public IHtmlCollection<IElement> getElementCollection()
+            {
+                return this.initIfNotNull(this.elementCollection, this.locatElementCollection);
+            }
+
+            // Абстракция парсинга элемента
+            public IEnumerable<T> Catch()
+            {
+                var elements = this.getElementCollection();
+
+                foreach (var el in elements)
+                {
+                    this.currentElement = el;
+
+                    T data = this.initDataFromElement(el);
+                    
+                    yield return data;
+                }
+            }
+
+            abstract protected IElement? locateTopElement(); // Поиск родителя с элементами
+            abstract protected IHtmlCollection<IElement>? locatElementCollection(); // Поиск конкретных элементов для парсинга
+            abstract protected T initDataFromElement(IElement element); // Парсинг элемента в Data объект класса <T>
+            
+            // Ленивая инициализация переменной с проверкой на null
+            private N initIfNotNull<N>(N? value, Func<N?> executeInitialization)
+            {
+                if (value is null) // Если не инициализированна
+                {
+                    value = executeInitialization();
+
+                    if (value is null) // Если не нашли элемент
                         throw new NullReferenceException("Top-Level Dom Element was not found");
 
-                    return this.topElement;
+                    return value;
                 }
-                return this.topElement;
+                return value;
             }
-            abstract protected IElement? locateTopElement();
-            abstract public IEnumerable<T> Catch();
         }
 
 #region CarsCatcher
