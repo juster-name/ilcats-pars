8506fe50ab4a7b9e10983944e32bff1bc3ef6698
juster-name <vladlen044@gmail.com>
2023-01-07 19:04:08 +0200

    ComplectationCatcher

--- Parser/Selectors.cs
+++ Parser/Selectors.cs
@@ -1,19 +1,29 @@
 using AngleSharp;
 using VladlenKazmiruk;
 
+
 namespace VladlenKazmiruk
 {
     namespace Parser
     {
         public static class Selectors
         {
-            public static string carTopLevelSelector = ".Multilist";
-            public static string carCellSelector = ":scope > div[class='List']"; // :scope для topLevelSelector
-            public static string carNameSelector = "div[class='name']";
-            public static string carInfoTopSelector = ":scope > div[class='List ']"; // : scope для carCellSelector
-            public static string carInfoSelector =  "div[class='List']";
-            public static string carDatesSelector = "div[class='dateRange']";
-            public static string carComplCodeSelector = "div[class='modelCode']";
+            public static string carTopLevel = ".Multilist";
+            public static string carCell = ":scope > div[class='List']"; // :scope для topLevel
+            public static string carName = "div[class='name']";
+
+            public static string carModelTop = ":scope > div[class='List ']"; // : scope для carCell
+            public static string carModel =  "div[class='List']";
+            public static string carModelDates = "div[class='dateRange']";
+            public static string carModelComplCode = "div[class='modelCode']";
+            
+            public static string complTop = "tbody";
+            public static string complCell = "tr:nth-child(n+2)";
+            public static string complDataNames = "th:nth-child(n+3)";
+            public static string complDataValues = "td:nth-child(n+3) div";
+
+            public static string dateRange = "div[class='dateRange']";
+            public static string code = "a";
         }
     }
 }
