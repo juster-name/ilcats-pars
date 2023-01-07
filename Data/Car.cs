c5d832faa38100173ea96afae4aa92fc3ae8fbe5
juster-name <vladlen044@gmail.com>
2023-01-07 19:02:29 +0200

    ToString

--- Data/Car.cs
+++ Data/Car.cs
@@ -12,6 +12,19 @@ namespace VladlenKazmiruk
             public IEnumerable<CarModel> Models { get => models; set => models = value; }
             private IEnumerable<CarModel> models = Enumerable.Empty<CarModel>();
             string? name = null;
+
+            public override string ToString()
+            {
+                var strW = new StringWriter();
+
+                strW.WriteLine($"{this.Name}");
+                 foreach (Data.CarModel carModel in this.Models)
+                 {
+                     strW.WriteLine($"\t{carModel.ToString()}");
+                 }
+
+                 return strW.ToString();
+            }
         }
     }
 }
--- Data/CarModel.cs
+++ Data/CarModel.cs
@@ -9,16 +9,28 @@ namespace VladlenKazmiruk
             public string? DateRange { get => dateRange; set => dateRange = value; }
             public string? ComplectationCode { get => complectationCode; set => complectationCode = value; }
             public string? Code { get => code; set => code = value; }
-            public IEnumerable<Complectation> Complectations { get => complectation; set => complectation = value; }
+            public IEnumerable<Complectation> Complectations { get => complectations; set => complectations = value; }
             public Car? Car { get => car; set => car = value; }
-            public string? Url { get => url; set => url = value; }
 
             string? code = null;
-            string? url = null;
             string? dateRange = null;
             string? complectationCode = null;
-            IEnumerable<Complectation> complectation = Enumerable.Empty<Complectation>();
+            IEnumerable<Complectation> complectations = Enumerable.Empty<Complectation>();
             Car? car;
-    }
+
+            public override string ToString()
+            {
+                var strW = new StringWriter();
+
+                  strW.WriteLine($"{this.Code} | {this.DateRange} | {this.ComplectationCode}");
+                  strW.WriteLine("Complectations:");
+                  foreach (var compl in this.complectations)
+                  {
+                    strW.WriteLine(compl.ToString());
+                  }
+
+                  return strW.ToString();
+            }
+        }
     }
 }
