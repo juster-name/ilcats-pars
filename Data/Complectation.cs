7090cc1bebb2e64380af0c886d37fa922a7b1ffa
juster-name <vladlen044@gmail.com>
2023-01-07 21:18:37 +0200

    Dictionary to List<KeyValuePair<string, string>>

--- Data/Complectation.cs
+++ Data/Complectation.cs
@@ -11,23 +11,25 @@ namespace VladlenKazmiruk
             public CarModel? CarModel { get => carModel; set => carModel = value; }
             public string? DateRange { get => dateRange; set => dateRange = value; }
             public string? Code { get => code; set => code = value; }
-            public Dictionary<string, string> Data { get => data; set => data = value; }
+            public List<KeyValuePair<string, string>>Data { get => data; set => data = value; }
+            public string? Url { get => url; set => url = value; }
 
             string? code;
             string? dateRange;
-            Dictionary<string, string> data = new Dictionary<string, string>();
+            // Dictionary не используем так как key может повторяться
+            List<KeyValuePair<string, string>> data = new List<KeyValuePair<string, string>> ();
 
             CarModel? carModel;
-
+            string? url = null;
             public override string ToString()
             {
                 var strW = new StringWriter();
 
-                strW.WriteLine($"Code: {this.Code}, Date: {this.DateRange}");
+                strW.WriteLine($"Code: {this.Code} | Date: {this.DateRange}");
                 strW.WriteLine("Data:");
                 foreach (var item in this.Data)
                 {
-                    strW.WriteLine($"\tkey = {item.Key} | Value = {item.Value}");
+                    strW.WriteLine($"\tkey = {item.Key}: {item.Value}");
                 }
                 return strW.ToString();
             }
