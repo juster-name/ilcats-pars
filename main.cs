315f9531e79f2009ad9c9cc25b5f92ebf3b67c16
juster-name <vladlen044@gmail.com>
2023-01-08 02:36:19 +0200

    sql queries

--- main.cs
+++ main.cs
@@ -13,11 +13,56 @@ namespace VladlenKazmiruk
 
         public static void Main(string[] args)
         {
+            var dbConnection = Test.SqlConnectOpen();
+            var transaction = dbConnection.BeginTransaction();
+            var command = new MySql.MySqlCommand(dbConnection, transaction);
+
+
+            dbConnection.Close();
             using (StreamWriter fileW = File.AppendText("carData.txt"))
             {
                 foreach (var car in Test.getCars())
                 {
-                    fileW.WriteLine(car.ToString());
+                    try
+                    {
+                        command.CommandText = $"INSERT INTO Car(name) VALUES('{car.Name}')";
+                        command.ExecuteNonQuery();
+                        
+                        foreach (var carModel in car.Models)
+                        {
+                        command.CommandText = 
+                        "INSERT INTO Model(code, date_prod_range, complectations, car_id)" +
+                         $"VALUES('{carModel.Code}', '{carModel.DateRange}', '{carModel.ComplectationCode}'," +
+                         $"(SELECT id FROM Car WHERE name='{car.Name}'))";
+                        command.ExecuteNonQuery();
+
+                            foreach (var compl in carModel.Complectations)
+                            {
+                                command.CommandText = 
+                                "INSERT INTO Complectations(date_prod_range, code, model_id" +
+                                $"VALUES('{compl.DateRange}', '{compl.Code}'," +
+                                $"(SELECT id FROM Model WHERE code='{carModel.Code}'))";
+                                command.ExecuteNonQuery();
+
+                                foreach (var data in compl.Data)
+                                {
+                                    command.CommandText =  
+                                    "INSERT INTO ComplectationData(data_name, data_value, complectation_id)" +
+                                    $"VALUES('{data.Key}', '{data.Value}'," +
+                                    $"(SELECT id FROM Complectations WHERE code='{compl.Code}'))";
+                                    command.ExecuteNonQuery();
+                                }
+                            }
+                        }
+
+                    transaction.Commit();
+                    }
+                    catch (Exception e)
+                    {
+                        Csl.WriteLine(e.Message);
+                        transaction.Rollback();
+                    }
+                    //fileW.WriteLine(car.ToString());
                 }
             }
         }
@@ -29,6 +74,7 @@ namespace VladlenKazmiruk
         {
             var config = Configuration.Default.WithDefaultLoader();
             var carsAddress = "https://www.ilcats.ru/toyota/?function=getModels&market=EU";
+            
             var context = BrowsingContext.New(config);
             var document = context.OpenAsync(carsAddress).WaitAsync(CancellationToken.None).Result;
 
@@ -70,7 +116,7 @@ namespace VladlenKazmiruk
             }
         }
 
-        static void SqlConnectionCheck()
+        public static MySql.MySqlConnection SqlConnectOpen()
         {
             string mysqlConnectionString = System.IO.File.ReadAllText("db-string.user");
 
@@ -87,7 +133,8 @@ namespace VladlenKazmiruk
                 Csl.Write("Denied");
 
             Csl.WriteLine("\n\t Closing connection...");
-            dbConnection.Close();
+
+            return dbConnection;
         }
     }
 }
