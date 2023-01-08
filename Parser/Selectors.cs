572911b98301f1bfdd92387638626e517ce5b3de
juster-name <vladlen044@gmail.com>
2023-01-08 06:39:37 +0200

    url dateRange parser

--- Parser/Selectors.cs
+++ Parser/Selectors.cs
@@ -25,5 +25,60 @@ namespace VladlenKazmiruk
             public static string dateRange = "div[class='dateRange']";
             public static string code = "a";
         }
+        static class Urls
+        {
+            public static string baseFull = "https://www.ilcats.ru";
+            public static string toyota= "/toyota";
+
+            public static string getCars = "/?function=getModels";
+            public static string getCompl = "/?function=getComplectations";
+
+            public static string marketArg = "&market=";
+            public static string modelArg = "&model=";
+            public static string startDateArg = "&startDate=";
+            public static string endDateArg = "&endDate=";
+            public static string yearArg = "$year=";
+
+            public static string formComplectationUrl(string carType, string market, string? idModel, string? dateRange)
+            {
+                if (idModel is null)
+                    throw new NullReferenceException("idModel must not be null to form an URL");
+
+                var dateArgs = dateRange is null ? "" : parseDateRangeToArgs(dateRange);
+
+                return baseFull + $"/{carType}" + getCompl + 
+                $"{marketArg}{market}" + $"{modelArg}{idModel}" + dateArgs;
+            }
+
+            public static string parseDateRangeToArgs(string dateRange)
+            {
+                int year = -1;
+                if(dateRange.Length == 4 && int.TryParse(dateRange, out year))
+                {
+                    return yearArg + year.ToString();
+                }
+
+                var dateStart = DateOnly.MinValue;
+                var dateEnd = DateOnly.MinValue;
+                var splitDates = dateRange.Split(separator: " - ");
+
+                DateOnly.TryParse(splitDates[0], out dateStart);
+
+                if (dateStart == DateOnly.MinValue)
+                    return "";
+
+                string retStr = startDateArg + dateStart.Year + 
+                    (dateStart.Month < 10 ? "0" + dateStart.Month : dateStart.Month);
+
+                if (splitDates.Length > 1)
+                    DateOnly.TryParse(splitDates[1], out dateEnd);
+                
+                if (dateEnd == DateOnly.MinValue)
+                    return retStr;
+
+                return retStr + endDateArg + dateEnd.Year +
+                    (dateEnd.Month < 10 ? "0" + dateEnd.Month : dateEnd.Month);;
+            }
+        }
     }
 }
