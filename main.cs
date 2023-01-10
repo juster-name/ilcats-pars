c65abd66e46f75c0872c962e31b606e2aead3619
juster-name <vladlen044@gmail.com>
2023-01-10 21:35:45 +0200

    test on mock data

--- main.cs
+++ main.cs
@@ -22,14 +22,58 @@ namespace VladlenKazmiruk
 
         public static void Main(string[] args)
         {
+            
+
             using (var connection = TestSql.SqlConnectOpen())
             {
-                foreach (var carModel in TestParse.ParseCar(ToyotaEuCar))
-                {
-                    TestSql.InsertIntoDb(connection, carModel);
-                }
-                connection.Close();
+                TestSql.InsertIntoDb(connection, getMockData());
+                
+                // foreach (var carModel in TestParse.ParseCar(ToyotaEuCar))
+                // {
+                //     TestSql.InsertIntoDb(connection, carModel);
+                // }
+                // Console.WriteLine("\n\t Closing connection...");
+                // connection.Close();
             }
+            
         }
+        public static Data.CarModel getMockData()
+            {
+                var cm = new Data.CarModel(){
+                    Name = "TestCarModel",
+                    Code = "11111",
+                    DateRange = "01.1990 - 04-1997",
+                    ComplectationCode = "2fg 4fd f# c5# gd#",
+                    Car = ToyotaEuCar,
+                    Url = "/testCarModelUrl",
+                    Complectation = new List<Data.Complectation>(1)
+                };
+                ((List<Data.Complectation>)cm.Complectation).Add(new Data.Complectation(){
+                    ModCode = "4F56-DFG1",
+                    DateRange = "01.1990 - 01-1991",
+                    CarModel = cm,
+                    Url = "/testComplUrl1",
+                    ModData = new List<KeyValuePair<string, string>>()
+                });
+                ((List<Data.Complectation>)cm.Complectation)[0].ModData.Add(new KeyValuePair<string, string>("ENGINE 1", "2L"));
+                ((List<Data.Complectation>)cm.Complectation)[0].ModData.Add(new KeyValuePair<string, string>("GRADE", "STD"));
+                ((List<Data.Complectation>)cm.Complectation)[0].ModData.Add(new KeyValuePair<string, string>("BODY", "T1"));
+                ((List<Data.Complectation>)cm.Complectation)[0].ModData.Add(new KeyValuePair<string, string>("ATM,MTM", "MTM"));
+
+                ((List<Data.Complectation>)cm.Complectation).Add(new Data.Complectation(){
+                    ModCode = "3A76-G6F2",
+                    DateRange = "02.1991 - 05-1992",
+                    CarModel = cm,
+                    Url = "/testComplUrl2",
+                    ModData = new List<KeyValuePair<string, string>>()
+                });
+                ((List<Data.Complectation>)cm.Complectation)[0].ModData.Add(new KeyValuePair<string, string>("ENGINE 1", "2L"));
+                ((List<Data.Complectation>)cm.Complectation)[0].ModData.Add(new KeyValuePair<string, string>("GRADE", "DLX"));
+                ((List<Data.Complectation>)cm.Complectation)[0].ModData.Add(new KeyValuePair<string, string>("BODY", "T1"));
+                ((List<Data.Complectation>)cm.Complectation)[0].ModData.Add(new KeyValuePair<string, string>("TRANSMISSION MODEL", "G"));
+                ((List<Data.Complectation>)cm.Complectation)[0].ModData.Add(new KeyValuePair<string, string>("LOADING CAPACITY", "HLF"));
+
+                return cm;
+            }
     }
 }
