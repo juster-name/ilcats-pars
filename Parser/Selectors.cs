595daa5d8ae6904c445a94a023ff845316e2f495
juster-name <vladlen044@gmail.com>
2023-01-10 13:59:09 +0200

    better url and year match

--- Parser/Selectors.cs
+++ Parser/Selectors.cs
@@ -1,6 +1,6 @@
 using AngleSharp;
 using VladlenKazmiruk;
-
+using System.Text.RegularExpressions;
 
 namespace VladlenKazmiruk
 {
@@ -9,8 +9,9 @@ namespace VladlenKazmiruk
         public static class Selectors
         {
             public static string carTopLevel = ".Multilist";
+            public static string carTopLevelV2 = ".List";
             public static string carCell = ":scope > div[class='List']"; // :scope для topLevel
-            public static string carName = "div[class='name']";
+            public static string carModelName = "div[class='name']";
 
             public static string carModelTop = ":scope > div[class='List ']"; // : scope для carCell
             public static string carModel =  "div[class='List']";
@@ -28,9 +29,9 @@ namespace VladlenKazmiruk
         static class Urls
         {
             public static string baseFull = "https://www.ilcats.ru";
-            public static string toyota= "/toyota";
+            //public static string toyota= "/toyota";
 
-            public static string getCars = "/?function=getModels";
+            public static string getModels = "/?function=getModels";
             public static string getCompl = "/?function=getComplectations";
 
             public static string marketArg = "&market=";
@@ -39,23 +40,36 @@ namespace VladlenKazmiruk
             public static string endDateArg = "&endDate=";
             public static string yearArg = "$year=";
 
-            public static string formComplectationUrl(string carType, string market, string? idModel, string? dateRange)
+            public static string languageArgRu = "$language=ru";
+
+            public static string formComplectationPath(string carName, string market, string? idModel, string? dateRange)
             {
                 if (idModel is null)
                     throw new NullReferenceException("idModel must not be null to form an URL");
 
                 var dateArgs = dateRange is null ? "" : parseDateRangeToArgs(dateRange);
 
-                return baseFull + $"/{carType}" + getCompl + 
+                return $"/{carName}" + getCompl + 
                 $"{marketArg}{market}" + $"{modelArg}{idModel}" + dateArgs;
+            }
+
+            public static string formCarModelsPath(string carName, string marketCode)
+            {
+                return $"/{carName}{getModels}{marketArg}{marketCode}";
             }
 
             public static string parseDateRangeToArgs(string dateRange)
             {
                 int year = -1;
-                if(dateRange.Length == 4 && int.TryParse(dateRange, out year))
+                if(dateRange.Length == 4 && int.TryParse(dateRange, out year)) // 2004
                 {
                     return yearArg + year.ToString();
+                }
+
+                var matches = Regex.Matches(dateRange, @"\d+");
+                if (matches.Count == 2 && matches[0].Length == 4 && matches[1].Length == 4) // Модельный год: 2009 - 2017
+                {
+                    return startDateArg + matches[0] + endDateArg + matches[1];
                 }
 
                 var dateStart = DateOnly.MinValue;
@@ -73,11 +87,11 @@ namespace VladlenKazmiruk
                 if (splitDates.Length > 1)
                     DateOnly.TryParse(splitDates[1], out dateEnd);
                 
-                if (dateEnd == DateOnly.MinValue)
+                if (dateEnd == DateOnly.MinValue) // 01.1998 - ...
                     return retStr;
 
                 return retStr + endDateArg + dateEnd.Year +
-                    (dateEnd.Month < 10 ? "0" + dateEnd.Month : dateEnd.Month);;
+                    (dateEnd.Month < 10 ? "0" + dateEnd.Month : dateEnd.Month);; // 02.1997 - 05.1999
             }
         }
     }
