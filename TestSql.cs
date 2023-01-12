278740fb28a2c8c21713bb9b78d56319739f559e
juster-name <vladlen044@gmail.com>
2023-01-12 12:07:45 +0200

    proper value @param insertion

--- TestSql.cs
+++ TestSql.cs
@@ -36,44 +36,63 @@ namespace VladlenKazmiruk
             }
             return commands[name];
         }
+        static void endTransaction()
+        {
+            commands.Clear();
+        }
 
         public static void InsertIntoDb(MySql.MySqlConnection connection, Data.CarModel carModel)
         {
+            if (carModel == null)
+                throw new NullReferenceException("Car Model must not be null when inserting into database");
+             if (carModel.Car == null)
+                throw new NullReferenceException("Car must not be null when inserting into database");
+
             log("Beginning database transaction.");
             var transaction = connection.BeginTransaction();
 
-            var modelComplsOrEmpty = Enumerable.Empty<Data.Complectation>();
-            if (carModel.Complectation == null)
-                log($"No Conplectation found in {carModel.Name}");
-            else
-                modelComplsOrEmpty = carModel.Complectation;
+            int carId = executeCarInsert(command("car", transaction), carModel.Car);
 
             try
             {
-                int modelId = executeModelInsert(transaction, carModel);
+                int modelNameId = executeModelNameInsert(command("modelName", transaction), carModel);
+                int modelId = executeModelInsert(command("model", transaction), carModel, carId, modelNameId);
 
-                foreach (var compl in modelComplsOrEmpty)
+                foreach (var compl in carModel.Complectation)
                 {
-                    int complId = executeComplectationInsert(transaction, compl, modelId);
+                    int complId = executeComplectationInsert(command("compl", transaction), compl, modelId);
                     foreach (var data in compl.ModData)
                     {
-                        int keyId = executeKeyInsert(transaction, data.Key);
-                        int valueId = executeValueInsert(transaction, data.Value);
+                        int keyId = executeKeyInsert(command("name", transaction), data.Key);
+                        int valueId = executeValueInsert(command("value", transaction), data.Value);
 
-                        int modId = executeModificationInsert(transaction, compl, complId);
-                        int dataPairId = executeDataPairInsert(transaction, modId, keyId, valueId);
+                        int modId = executeModificationInsert(command("mod", transaction), compl, complId);
+                        int dataPairId = executeDataPairInsert(command("dataPair", transaction), modId, keyId, valueId);
                     }
                 }
-                log("Commiting transaction.");
-                transaction.Commit();
             }
             catch (Exception e)
             {
                 log(e.Message);
-                 log("Rolling back");
+                log("Rolling back");
                 transaction.Rollback();
             }
-            log("Transaction commited successfully.");
+
+            try
+            {
+                log("Commiting transaction.");
+                transaction.Commit();
+                log("Transaction commited successfully.");
+            }
+            catch(Exception e)
+            {
+                log("Transaction FAILED.");
+                log(e.Message);
+            }
+            finally
+            {
+                endTransaction();
+            }
         }
 
         static public void log(string arg)
@@ -86,22 +105,74 @@ namespace VladlenKazmiruk
             Console.WriteLine(DateTime.Now.ToLocalTime() + $": Querying {name}");
         }
 
-        static int insert(MySql.MySqlCommand command, string tableName, string column, string value)
+        static Dictionary<string,string> addParams(MySql.MySqlCommand command, int len, List<string> values)
         {
+            command.Parameters.Clear();
+            var paramsDict = formParamsDict(len, values);
+
+            foreach (var item in paramsDict)
+                command.Parameters.AddWithValue(item.Key, item.Value);
+
+            return paramsDict;
+        }
+
+        static Dictionary<string, string> formParamsDict(int len, List<string> values)
+        {
+            var pKeys = parseParams(len);
+            var pValues = formValues(values);
+
+            return Enumerable.Range(0, len).ToDictionary(i => pKeys[i], i => pValues[i]);
+        }
+
+        static List<string> parseParams(int paramsN)
+        {
+            var retList = new List<string>();
+
+            for (int i = 0; i < paramsN; i++)
+            {
+                retList.Add($"@param{i}");
+            }
+
+            return retList;
+        }
+
+        static List<string>  formValues(List<string> paramValues)
+        {
+            paramValues.ForEach(x => x.Trim());
+            return paramValues;
+        }
+
+        static int insertOrSelect(MySql.MySqlCommand command, string tableName, 
+            string columnsArg, List<string> values, string where, List<string> equals)
+        {
+            var lastId = insert(command, tableName, columnsArg, values);
+
+            if (lastId == 0)
+                return selectIdWhere(command, tableName, where, equals);
+
+            return lastId;
+        }
+
+        static int insert(MySql.MySqlCommand command, string tableName, string columnArg, List<string> values)
+        {
+            var paramsDict = addParams(command, values.Count, values);
+
             command.CommandText =
-            $"INSERT IGNORE INTO {tableName}({column}) " +
-            $"VALUES({value})";
+            $"INSERT IGNORE INTO {tableName}({columnArg}) " +
+            $"VALUES({String.Join(',', paramsDict.Keys)})";
 
             logQueryCommand(command.CommandText);
-
             command.ExecuteNonQuery();
 
             return ((int)command.LastInsertedId);
         }
 
-        static int selectIdWhere(MySql.MySqlCommand command, string tableName, string whereColumn, string whereEquals)
+        static int selectIdWhere(MySql.MySqlCommand command, string tableName, string where, List<string> equals)
         {
-            command.CommandText= $"SELECT id FROM {tableName} WHERE ({whereColumn}) = ({whereEquals})";
+            var paramsDict = addParams(command, equals.Count, equals);
+
+            command.CommandText= $"SELECT id FROM {tableName} " + 
+            $"WHERE ({where}) = ({String.Join(',', paramsDict.Keys)})";
 
             logQueryCommand(command.CommandText);
 
@@ -112,85 +183,97 @@ namespace VladlenKazmiruk
             return ((int)value);
         }
 
-        static int insertOrSelect(MySql.MySqlCommand command, string tableName, 
-            string columnsArg, string valuesArg, string where, string equals)
+        static List<string> valueArray(params string?[] args)
         {
-            var lastId = insert(command, tableName, columnsArg, valuesArg);
-
-
-            if (lastId == 0)
-                return selectIdWhere(command, tableName, where, equals);
-
-            return lastId;
+            return new List<string>(args.Select(val => val = (val == null ? "NULL" : val)));
         }
 
-        static int executeModelInsert(MySql.MySqlTransaction transaction, Data.CarModel carModel)
+        static int executeCarInsert(MySql.MySqlCommand command, Data.Car car)
         {
-            if (carModel == null)
-                throw new NullReferenceException("Car Model must be assigned before adding to database");
+            if (car.Name == null)
+                throw new NullReferenceException();
 
+            return insertOrSelect(command, 
+                tableName:"Car", 
+                columnsArg: "name, market_code, market_name",
+                values: valueArray(car.Name, $"{car.MarketCode}", car.Market), 
+                where: "name", 
+                equals: valueArray(car.Name));
+        }
+
+        static int executeModelNameInsert(MySql.MySqlCommand command, Data.CarModel carModel)
+        {
+            return insertOrSelect(command,
+                tableName:"ModelName", 
+                columnsArg: "name",
+                values: valueArray(carModel.Name),
+                where:"name", 
+                equals: valueArray(carModel.Name));
+        }
+
+        static int executeModelInsert(MySql.MySqlCommand command, Data.CarModel carModel, int carId, int nameId)
+        {
             string? carNameOrNull = "NULL";
 
             if (carModel.Car != null && carModel.Car.Name != null)
                     carNameOrNull = carModel.Car.Name;
 
-            return insertOrSelect(command("model", transaction), 
+            return insertOrSelect(command, 
                 tableName:"Model", 
-                columnsArg: "code, name, car_id",
-                valuesArg: $"'{carModel.Code}','{carModel.Name}', " +
-                    $"(SELECT id FROM Car WHERE name='{carNameOrNull}')",
+                columnsArg: "code, name, car_id, model_name_id",
+                values: valueArray($"{carModel.Code}", carModel.Name, $"{carId}", $"{nameId}"),
                 where:"name, code", 
-                equals:$"'{carModel.Name}', '{carModel.Code}'");
+                equals: valueArray(carModel.Name, $"{carModel.Code}"));
         }
 
-        static int executeComplectationInsert(MySql.MySqlTransaction transaction, Data.Complectation compl, int modelIdLink)
+        static int executeComplectationInsert(MySql.MySqlCommand command, Data.Complectation compl, int modelIdLink)
         {
-            return insertOrSelect(command("compl", transaction), 
+            return insertOrSelect(command, 
                 tableName:"Complectation", 
                 columnsArg: "date_prod_range, code, model_id",
-                valuesArg: $"'{compl.CarModel?.DateRange}', '{compl.CarModel?.ComplectationCode}','{modelIdLink}'", 
+                values: valueArray($"{compl.CarModel?.DateRange}", $"{compl.CarModel?.ComplectationCode}", $"{modelIdLink}"), 
                 where:"code, model_id", 
-                equals:$"'{compl.CarModel?.ComplectationCode}', '{modelIdLink}'");
+                equals:valueArray($"{compl.CarModel?.ComplectationCode}", $"{modelIdLink}"));
         }
 
-        static int executeModificationInsert(MySql.MySqlTransaction transaction, Data.Complectation compl, int compllIdLink)
+        static int executeModificationInsert(MySql.MySqlCommand command, Data.Complectation compl, int compllIdLink)
         {
-            return insertOrSelect(command("mod", transaction), 
+            return insertOrSelect(command, 
                 tableName:"Modification", 
                 columnsArg:"code, complectation_id, date_prod_range", 
-                valuesArg: $"'{compl.ModCode}', '{compllIdLink}', '{compl.DateRange}'", 
+                values: valueArray($"{compl.ModCode}", $"{compllIdLink}", $"{compl.DateRange}"), 
                 where:"code, complectation_id", 
-                equals:$"'{compl.ModCode}', '{compllIdLink}'");
+                equals:valueArray($"{compl.ModCode}", $"{compllIdLink}"));
         }
 
-        static int executeKeyInsert(MySql.MySqlTransaction transaction, string dataKey)
+        static int executeKeyInsert(MySql.MySqlCommand command, string dataKey)
         {
-            return insertOrSelect(command("name", transaction), 
+            return insertOrSelect(command, 
                 tableName:"ModDataName", 
                 columnsArg:"name", 
-                valuesArg:$"'{dataKey}'", 
+                values:valueArray($"{dataKey}"), 
                 where:"name", 
-                equals:$"'{dataKey}'");
+                equals:valueArray($"{dataKey}"));
         }
 
-        static int executeValueInsert(MySql.MySqlTransaction transaction, string dataValue)
+        static int executeValueInsert(MySql.MySqlCommand command, string dataValue)
         {
-            return insertOrSelect(command("value", transaction), 
+            return insertOrSelect(command, 
                 tableName:"ModDataValue", 
                 columnsArg:"value", 
-                valuesArg: $"'{dataValue}'", 
+                values: valueArray($"{dataValue}"), 
                 where:"value", 
-                equals:$"'{dataValue}'");
+                equals:valueArray($"{dataValue}"));
         }
 
-        static int executeDataPairInsert(MySql.MySqlTransaction transaction, int modId, int keyId, int valueId)
+        static int executeDataPairInsert(MySql.MySqlCommand command, int modId, int keyId, int valueId)
         {
-            return insertOrSelect(command("dataPair", transaction), 
+            return insertOrSelect(command, 
                 tableName:"ModDataPair",
                 columnsArg:"modification_id, name_id, value_id", 
-                valuesArg:$"'{modId}', '{keyId}', '{valueId}'", 
+                values:valueArray($"{modId}", $"{keyId}", $"{valueId}"), 
                 where:"modification_id, name_id, value_id", 
-                equals:$"'{modId}', '{keyId}', '{valueId}'");
+                equals:valueArray($"{modId}", $"{keyId}", $"{valueId}"));
             
         }
     }
